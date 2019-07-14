57c52497cf14e17260c6e6d2372af78eb7a048c4
yohanes_prima <jooprima@gmail.com>
2019-07-14 22:38:15 +0700

    Tugas 7 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+var angka = [134, 153, 121, 156, 123, 178, 165, 155, 152, 146];
+
+for (var i = 0; i < angka.length; i++) {
+  console.log(angka[i]);
+}
+
+for (let x of angka) {
+  console.log(x);
+}
